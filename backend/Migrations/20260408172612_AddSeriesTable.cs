f0faba1d5069cf9acae05398a66a2094dbd1a5ce
Diego <devgomezdiego@gmail.com>
2026-04-08 19:26:39 +0200

    feat: añadida tabla de series al contexto

---
+++ backend/Migrations/20260408172612_AddSeriesTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Series",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    Creador = table.Column<string>(type: "TEXT", nullable: false),
+                    Sinopsis = table.Column<string>(type: "TEXT", nullable: false),
+                    Anio = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Series", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Series");
+        }
+    }
+}
